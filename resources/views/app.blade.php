641cf42228cf0aea01835a4a0ff9506ba69607f6
ariel4100 <arielcallisaya00@gmail.com>
2022-05-23 19:53:43 -0300

    app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}?=6" defer></script>
+        <script src="{{ mix('js/app.js') }}?=7" defer></script>
         <!-- Font Awesome -->
         <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
